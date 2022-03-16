d765968e1ea828e911b73a2b2eedd9595bd6c97f
Rupesh Kumar Hazari <rupeshhazari9168@gmail.com>
2022-03-17 04:02:50 +0530

    Advance JS

---
+++ js/library.js
@@ -0,0 +1,19 @@
+// export default function(){
+//     console.log("Hello");
+// }
+
+// let message = "ES6 Modules";
+
+function user(name) {
+    return `Hello ${name}`;
+}
+
+export {user};
+
+// class test{
+//     constructor(){
+//         console.log("Constructor Method");
+//     }
+// }
+
+// export {message, user, test};
---
+++ js/main.js
@@ -0,0 +1,19 @@
+import {user} from "./bridge.js";
+
+console.log(user("Rupesh"));
+
+// import {default as yahoo} from "./library.js";
+// import {message, user as us, test} from "./library.js";
+
+// yahoo();
+
+// console.log(message);
+// console.log(us("Rupesh"));
+// let a = new test();
+
+// import * as yahoo from "./library.js";
+
+// console.log(yahoo.message);
+// console.log(yahoo.user("Rupesh"));
+// let a = new yahoo.test();
+
